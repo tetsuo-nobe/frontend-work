3fed9b8f19daf56f489a3313e3151bff7f60f569
Tetsuo Nobe <69065590+tetsuo-nobe@users.noreply.github.com>
2026-04-23 15:14:11 +0900

    Delete example-apps/mod5 directory

--- example-apps/mod5/4-frontend/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: ['ec2-xx-xxx-xxx-xx.ap-northeast-1.compute.amazonaws.com']
-};
-
-export default nextConfig;
